fccb32990fb21e82d8e8ccc7e96454e1fcefb39d
mo khan <mo@mokhan.ca>
2013-07-10 20:58:48 -0600

    implement insertion sort

--- spec/insertion_sort_spec.rb
+++ spec/insertion_sort_spec.rb
@@ -1,12 +1,22 @@
 require "spec_helper"
 
 describe InsertionSort do
+  let(:sut) { InsertionSort.new }
+
   it "should sort an empty array" do
-    InsertionSort.new.sort([]).should == []
+    sut.sort([]).should == []
   end
 
   it "should sort an array with one item" do
-    InsertionSort.new.sort([1]).should == [1]
+    sut.sort([1]).should == [1]
+  end
+
+  it "should sort an array of numbers" do
+    n = 200
+    numbers = Array.new(n) { rand(n) }
+    Benchmark.bmbm do |x|
+      x.report("insertion sort") { sut.sort(numbers).should == numbers.sort }
+    end
   end
 end
 
