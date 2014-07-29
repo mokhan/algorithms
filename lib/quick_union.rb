91cc49048409f9dc6bbf28bf520de43c533e612f
mo khan <mo@mokhan.ca>
2014-07-28 20:23:10 -0600

    optimize performance of quick union by maintaining balance.

--- lib/quick_union.rb
+++ lib/quick_union.rb
@@ -1,10 +1,21 @@
 class QuickUnion
   def initialize(size)
     @items = Array.new(size) { |n| n }
+    @size = Array.new(size, 0)
   end
 
   def union(x, y)
-    @items[x] = root_of(y)
+    i = root_of(x)
+    j = root_of(y)
+    return if (i == j)
+
+    if @size[i] < @size[j]
+      @items[i] = j
+      @size[j] = @size[j] + @size[i]
+    else
+      @items[j] = i
+      @size[i] = @size[i] + @size[j]
+    end
   end
 
   def connected?(x, y)
