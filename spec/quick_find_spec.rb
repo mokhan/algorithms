88f4f92ca7ab2be03732a1f622af68068932d25b
mo khan <mo@mokhan.ca>
2015-01-24 16:31:28 -0700

    upgrade specs.

--- spec/quick_find_spec.rb
+++ spec/quick_find_spec.rb
@@ -5,40 +5,40 @@ describe QuickFind do
 
   it "is not connected by default" do
     10.times do |n|
-      expect(subject.connected?(n, n+1)).to be_false
+      expect(subject.connected?(n, n+1)).to be_falsey
     end
   end
 
   it "is connected" do
     subject.union(0, 1)
-    expect(subject.connected?(0, 1)).to be_true
+    expect(subject.connected?(0, 1)).to be_truthy
   end
 
   it "unions and connects properly" do
     subject.union(4, 3)
-    expect(subject.connected?(4, 3)).to be_true
-    expect(subject.connected?(3, 4)).to be_true
+    expect(subject.connected?(4, 3)).to be_truthy
+    expect(subject.connected?(3, 4)).to be_truthy
 
     subject.union(3, 8)
-    expect(subject.connected?(3, 8)).to be_true
+    expect(subject.connected?(3, 8)).to be_truthy
 
     subject.union(6, 5)
-    expect(subject.connected?(6, 5)).to be_true
+    expect(subject.connected?(6, 5)).to be_truthy
 
     subject.union(9, 4)
-    expect(subject.connected?(9, 4)).to be_true
+    expect(subject.connected?(9, 4)).to be_truthy
 
     subject.union(2, 1)
-    expect(subject.connected?(2, 1)).to be_true
+    expect(subject.connected?(2, 1)).to be_truthy
 
     subject.union(8, 9)
-    expect(subject.connected?(8, 9)).to be_true
+    expect(subject.connected?(8, 9)).to be_truthy
 
-    expect(subject.connected?(5, 0)).to be_false
+    expect(subject.connected?(5, 0)).to be_falsey
     subject.union(5, 0)
-    expect(subject.connected?(5, 0)).to be_true
-    expect(subject.connected?(6, 0)).to be_true
-    expect(subject.connected?(5, 6)).to be_true
+    expect(subject.connected?(5, 0)).to be_truthy
+    expect(subject.connected?(6, 0)).to be_truthy
+    expect(subject.connected?(5, 6)).to be_truthy
 
     subject.union(7, 2)
     subject.union(6, 1)
@@ -54,7 +54,7 @@ describe QuickFind do
       [4, 9],
     ]
     tuples.each do |tuple|
-      expect(subject.connected?(tuple.first, tuple.last)).to be_true
+      expect(subject.connected?(tuple.first, tuple.last)).to be_truthy
     end
   end
 end
--- spec/quick_union_spec.rb
+++ spec/quick_union_spec.rb
@@ -4,30 +4,30 @@ describe QuickUnion do
   subject { QuickUnion.new(10) }
 
   it "is not connected" do
-    expect(subject.connected?(0, 1)).to be_false
+    expect(subject.connected?(0, 1)).to be_falsey
   end
 
   it "is connected" do
     subject.union(4, 3)
-    subject.connected?(4, 3).should be_true
+    expect(subject.connected?(4, 3)).to be_truthy
     subject.union(3, 8)
-    subject.connected?(3, 8).should be_true
+    expect(subject.connected?(3, 8)).to be_truthy
     subject.union(6, 5)
-    subject.connected?(6, 5).should be_true
+    expect(subject.connected?(6, 5)).to be_truthy
 
     subject.union(9, 4)
-    subject.connected?(9, 4).should be_true
+    expect(subject.connected?(9, 4)).to be_truthy
 
     subject.union(2, 1)
-    subject.connected?(8, 9).should be_true
-    subject.connected?(5, 4).should be_false
+    expect(subject.connected?(8, 9)).to be_truthy
+    expect(subject.connected?(5, 4)).to be_falsey
     subject.union(5, 0)
-    subject.connected?(5, 0).should be_true
+    expect(subject.connected?(5, 0)).to be_truthy
     subject.union(7, 2)
-    subject.connected?(7, 2).should be_true
+    expect(subject.connected?(7, 2)).to be_truthy
     subject.union(6, 1)
-    subject.connected?(6, 1).should be_true
+    expect(subject.connected?(6, 1)).to be_truthy
     subject.union(7, 3)
-    subject.connected?(7, 3).should be_true
+    expect(subject.connected?(7, 3)).to be_truthy
   end
 end
