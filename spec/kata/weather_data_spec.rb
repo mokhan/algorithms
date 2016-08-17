580069153fc05d8a906c7f6587b5ac88ddfe8b74
mo khan <mo@mokhan.ca>
2016-08-16 20:50:36 -0600

    football.dat

--- spec/kata/weather_data_spec.rb
+++ spec/kata/weather_data_spec.rb
@@ -5,6 +5,25 @@ require 'ostruct'
 #Download this text file, then write a program to output the day number (column one) 
 #with the smallest temperature spread (the maximum temperature is the second column, the minimum the third column).
 
+class Team
+  include Comparable
+  attr_reader :spread, :name
+
+  def initialize(name:, goals_for:, goals_against:)
+    @name = name
+    @spread = (goals_for - goals_against).abs
+  end
+
+  def <=>(other)
+    @spread <=> other.spread
+  end
+
+  def self.map_from(row)
+    return nil if row[1].nil?
+    Team.new(name: row[1], goals_for: row[6].to_i, goals_against: row[8].to_i)
+  end
+end
+
 class Day
   include Comparable
   attr_reader :day, :spread
@@ -36,7 +55,9 @@ class Spread
       file.each_line.with_index do |line, index|
         next if index == 0
         next if line.chomp.empty?
-        yield mapper.map_from(line.split)
+        row =  mapper.map_from(line.split)
+        next if row.nil?
+        yield row
       end
     end
   end
@@ -53,10 +74,22 @@ class Spread
   attr_reader :file_path, :mapper
 end
 
-describe "spread" do
+describe "weather.dat" do
   subject { Spread.new('weather.dat', Day) }
 
   it 'returns the day with the smallest temperature spread' do
     expect(subject.min.day).to eql('14')
+  end
+end
+
+# Part Two: Soccer League Table
+# The file football.dat contains the results from the English Premier League for 2001/2. 
+# The columns labeled ‘F’ and ‘A’ contain the total number of goals scored for and against each team in that season (so Arsenal scored 79 goals against opponents, and had 36 goals scored against them). Write a program to print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+#
+describe "football.dat" do
+  subject { Spread.new('football.dat', Team) }
+
+  it 'returns the day with the smallest temperature spread' do
+    expect(subject.min.name).to eql('Aston_Villa')
   end
 end
